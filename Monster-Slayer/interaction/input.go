00c690ed063e347d4263a3d2320e3adcaf1822bf
johnmerga <johnmerga@yahoo.com>
2023-09-14 13:30:16 +0300

    (fix): user input error fixed for windows version

--- Monster-Slayer/interaction/input.go
+++ Monster-Slayer/interaction/input.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"errors"
 	"os"
+
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -15,18 +17,22 @@ func UserInput(isSpecial bool) (int8, error) {
 	if err != nil {
 		return -1, err
 	}
-	input = strings.Replace(input, "\n", "", -1)
+	endingLine := "\n"
+	if runtime.GOOS == "windows" {
+		endingLine = "\r\n"
+	}
+	input = strings.Replace(input, endingLine, "", -1)
 	// parse the string into int8
 	parsedInput, err := strconv.ParseInt(input, 10, 8)
 	if err != nil {
-		return -1, errors.New("Please Only type numbers")
+		return -1, errors.New("please Only type numbers")
 	}
 	selectedNum := int8(parsedInput)
 	if selectedNum <= 0 || selectedNum >= 4 {
-		return -1, errors.New("The Number Should be (1),(2) or (3) if it's available \n")
+		return -1, errors.New("the number should be (1),(2) or (3) if it's available")
 	}
 	if !isSpecial && selectedNum == 3 {
-		return -1, errors.New("The Special Attack is not Available yet.\n")
+		return -1, errors.New("the Special attack is not Available yet")
 	}
 	return selectedNum, nil
 }
