44a2d17f8265eab5e0bb883de3540df175c91e31
johnmerga <johnmerga@yahoo.com>
2023-12-05 15:00:12 +0300

    :recycle: Minro update for testing

--- snippetbox/internal/assert/assert.go
+++ snippetbox/internal/assert/assert.go
@@ -9,6 +9,6 @@ func Equals[T comparable](t *testing.T, result, expected T) {
 	// number of the code which called our Equal() function in the output.
 	t.Helper()
 	if result != expected {
-		t.Errorf("\nResult %T; Expected %T", result, expected)
+		t.Errorf("\nResult %v\nExpected %v", result, expected)
 	}
 }
